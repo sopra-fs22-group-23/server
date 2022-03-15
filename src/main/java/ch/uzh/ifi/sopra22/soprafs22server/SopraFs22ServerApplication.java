70679553e0f179328a5b250a8e76ac2b1583662a
bauerDOTuzh <adam.bauer@uzh.ch>
2022-03-15 15:49:30 +0100

    init

---
+++ src/main/java/ch/uzh/ifi/sopra22/soprafs22server/SopraFs22ServerApplication.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.sopra22.soprafs22server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopraFs22ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopraFs22ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/uzh/ifi/sopra22/soprafs22server/SopraFs22ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.sopra22.soprafs22server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopraFs22ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
