c7f06615e1ddb6e2dfc053ba576132960c259c80
bauerDOTuzh <adam.bauer@uzh.ch>
2022-03-15 19:09:53 +0100

    depemdencies added

--- src/main/java/ch/uzh/ifi/sopra22/soprafs22server/Application.java
+++ src/main/java/ch/uzh/ifi/sopra22/soprafs22server/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SopraFs22ServerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SopraFs22ServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
