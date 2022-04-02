0011b16858788730a5c4b34084c124b68260cf37
wmueri13 <wesley.mueri@uzh.ch>
2022-04-02 11:58:39 +0200

    EventUserId

--- src/main/java/ch/uzh/ifi/sopra22/entity/EventUserId.java
+++ src/main/java/ch/uzh/ifi/sopra22/entity/EventUserId.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.sopra22.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 
 public class EventUserId implements Serializable {
@@ -14,4 +15,32 @@ public class EventUserId implements Serializable {
         this.eventId = eventId;
     }
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    @Override
+    public boolean equals(Object eventUserId) {
+        if (this == eventUserId) return true;
+        if (!(eventUserId instanceof EventUserId)) return false;
+        return Objects.equals(getEventId(), ((EventUserId) eventUserId).getEventId()) &&
+                Objects.equals(getUserId(), ((EventUserId) eventUserId).getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.eventId.toString() + "-" + this.userId.toString());
+    }
 }
