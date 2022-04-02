96f893f305a0724a5c34f5e6adb8a8de127dc79a
bauerDOTuzh <adam.bauer@uzh.ch>
2022-04-02 19:55:03 +0200

    Websockets working in room

--- src/main/java/ch/uzh/ifi/sopra22/websockets/SessionSchedulerController.java
+++ src/main/java/ch/uzh/ifi/sopra22/websockets/SessionSchedulerController.java
@@ -4,6 +4,7 @@ import ch.uzh.ifi.sopra22.websockets.entities.NotificationMessage;
 import ch.uzh.ifi.sopra22.websockets.entities.OutputMessage;
 import ch.uzh.ifi.sopra22.websockets.entities.TaskMessage;
 import ch.uzh.ifi.sopra22.websockets.entities.enums.WebsocketType;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -20,10 +21,10 @@ public class SessionSchedulerController {
     /**
      * This is just redirection of an message to all other
      */
-    @MessageMapping("/sessionScheduler")
-    @SendTo("/topic/sessionScheduler")
-    public TaskMessage send(final TaskMessage message){
+    @MessageMapping("/sessionScheduler/{eventHash}")
+    @SendTo("/topic/sessionScheduler/{eventHash}")
+    public TaskMessage send(@DestinationVariable("eventHash") String eventHash, final TaskMessage message){
         System.out.println(message.toString());
-        return new TaskMessage(message.getTaskID(), message.getUser(), message.getAction());
+        return new TaskMessage(message.getTaskID(), message.getUser().concat("With hash:").concat(eventHash), message.getAction());
     }
 }
