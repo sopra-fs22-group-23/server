49b625b59dbb1fa71671483cc661545c70edd2f0
Kai Zinnhardt <kai.zinnhardt@uzh.ch>
2022-04-29 17:14:59 +0200

    Edited GmailConfig

--- src/main/java/ch/uzh/ifi/sopra22/mail/GmailConfig.java
+++ src/main/java/ch/uzh/ifi/sopra22/mail/GmailConfig.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.sopra22.mail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,14 +10,20 @@ import java.util.Properties;
 
 @Configuration
 public class GmailConfig {
+    @Value("${mail.support.username}")
+    private String userName;
+    @Value("${mail.support.password}")
+    private String password;
     @Bean("gmail")
     public JavaMailSender gmailMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("wevent21@gmail.com");
-        mailSender.setPassword("Wevent$Group$23");
+        /**mailSender.setUsername("wevent21@gmail.com");
+        mailSender.setPassword("Wevent$Group$23");*/
+        mailSender.setUsername(userName);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
