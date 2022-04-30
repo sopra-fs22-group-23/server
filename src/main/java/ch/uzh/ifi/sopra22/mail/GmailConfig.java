4c7a37531ae8fdde606a83e4944a95d95a654e19
Kai Zinnhardt <kai.zinnhardt@uzh.ch>
2022-04-30 10:31:34 +0200

    Takes credentials from local config file

--- src/main/java/ch/uzh/ifi/sopra22/mail/GmailConfig.java
+++ src/main/java/ch/uzh/ifi/sopra22/mail/GmailConfig.java
@@ -1,8 +1,10 @@
 package ch.uzh.ifi.sopra22.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -10,10 +12,14 @@ import java.util.Properties;
 
 @Configuration
 public class GmailConfig {
+    @Autowired
+    private Environment environment;
+
+    /**
     @Value("${mail.support.username}")
     private String userName;
     @Value("${mail.support.password}")
-    private String password;
+    private String password;*/
     @Bean("gmail")
     public JavaMailSender gmailMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -22,8 +28,11 @@ public class GmailConfig {
 
         /**mailSender.setUsername("wevent21@gmail.com");
         mailSender.setPassword("Wevent$Group$23");*/
-        mailSender.setUsername(userName);
-        mailSender.setPassword(password);
+        //takes credentials locally from my config file application.properties
+        mailSender.setUsername(environment.getProperty("spring.mail.username"));
+        System.out.println("Username: "+environment.getProperty("spring.mail.username"));
+        mailSender.setPassword(environment.getProperty("spring.mail.password"));
+        System.out.println("Password: "+environment.getProperty("spring.mail.password"));
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
